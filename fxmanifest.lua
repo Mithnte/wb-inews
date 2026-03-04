53ed1726022f6b562b4654e9cf6565481d27ae96
bae <baehareq@gmail.com>
2026-03-04 16:05:07 +0900

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,22 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'By By & Bae'
+description 'Job reporter Roleplay - Maksimal, Lightweight, Usable'
+version '6.1'
+
+shared_scripts { 'config.lua' }
+
+client_scripts { 'client.lua' }
+
+server_scripts { 'server.lua' }
+
+ui_page 'html/index.html'
+
+files {
+    'html/index.html',
+    'html/style.css',
+    'html/script.js'
+}
+
+lua54 'yes'
